f4a5de6b3f14da272226fc98d436f1d01c9142a9
Maksim Tsepkov <max@dappvibe.com>
2026-01-25 06:31:38 +0500

    actually replace INGEST_KEY in config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-envsubst '${TWITTER_STREAM_KEY} ${TELEGRAM_STREAM_KEY} ${YOUTUBE_STREAM_KEY}' \
+envsubst '${INGEST_KEY} ${TWITTER_STREAM_KEY} ${TELEGRAM_STREAM_KEY} ${YOUTUBE_STREAM_KEY}' \
   < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
